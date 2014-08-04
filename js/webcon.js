3aab7403f1d6ea8798b0455b0a525e29d8c0e7e4
y-nakazawa <y_nakazawa@jsl.co.jp>
2014-08-05 01:10:58 +0900

    x-youtubeタグの埋め込み

--- js/webcon.js
+++ js/webcon.js
@@ -1,12 +1,23 @@
 
 // x-youtube タグを作る
 var XYoutubeProtoType = Object.create(HTMLElement.prototype, {
-	createCallBack: {
+	createdCallback: {
 		value: function() {
 			// templateタグを取得する
 			var template = document.querySelector('#x-youtube-template');
 			// templateのDocumentFragmetからcontentを参照し取得する
 			var content = template.content;
+
+			// iframeのsrc属性を入れ替える
+			var video = content.querySelector('iframe');
+			var videoUrl = "http://www.youtube.com/embed/";
+			// カスタム要素の属性値を変更する
+			var videoId = this.getAttribute('video-id')
+			var videoSrc = videoUrl + videoId;
+
+			// 作られたiframeをsrc属性に設定する
+			video.setAttribute('src', videoSrc);
+
 			// Shadow Rootにcontentを追加する
 			this.createShadowRoot().appendChild(
 				document.importNode(content, true)
@@ -16,33 +27,6 @@ var XYoutubeProtoType = Object.create(HTMLElement.prototype, {
 });
 
 // ライフタイムコールバックの登録
-XYoutubeProtoType.createCallBack = function() {
-	console.log('XYoutubeが生成された');
-/*
-	value: function() {
-		// templateタグを取得する
-		var template = document.querySelector('#x-youtube-template');
-		// templateのDocumentFragmetからcontentを参照し取得する
-		var content = template.content;
-		// Shadow Rootにcontentを追加する
-		this.createShadowRoot().appendChild(
-			document.importNode(content, true);
-		);
-	}
-*/
-	// mouseover時のイベントを指定
-	this.addEventListener('#mouseover', function(event) {
-		this.style.color = '#fff';
-		this.style.backgroundColor = '#000';
-	});
-
-	// mouseout時のイベントを指定
-	this.addEventListener('#mouseout', function(event) {
-		this.style.color = '#000';
-		this.style.backgroundColor = '#fff';
-	});
-};
-
 XYoutubeProtoType.attachedCallBack = function() {
 	console.log('XYoutubeが追加された');
 };
@@ -55,6 +39,4 @@ XYoutubeProtoType.atributeChangedCallBack = function() {
 	console.log('XYoutubeの属性が変化した');
 };
 
-var XYoutube = document.registerElement('x-youtube',{
-	prototype: XYoutubeProtoType
-});
+var XYoutube = document.registerElement('x-youtube', {prototype: XYoutubeProtoType});
